f88ea3449b1685e3152a34d2f358dda3526a95ee
yavnek <jonatan.gasiorowski@gmail.com>
2023-06-14 20:28:36 +0200

    po instalacji lintera

---
+++ index.js
@@ -0,0 +1,16 @@
+// importuje modul express
+const express = require("express"); // import express
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.send("Hello World!");
+});
+
+// Uruchamia serwer nasłuchujący
+// na porcie 3000.
+app.listen(port, () => {
+   console.log(`Example app listening on port ${port}!`);
+});
